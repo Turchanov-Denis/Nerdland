88467fdb14df676349b0781c7c7ee6a22df9a852
Turchanov-Denis <turchanovdenis786@gmail.com>
2026-06-14 21:27:13 +0200

    init db

--- services/account/cmd/main.go
+++ services/account/cmd/main.go
@@ -1,11 +1,40 @@
 package main
 
-import "account/internal/logger"
+import (
+	"account/internal/database"
+	"account/internal/logger"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
 
 func main() {
 	log := logger.New()
-	log.Info("Nerdland")
+	log.Info("The server is running, but it still attracts nerds.")
+
+	err := godotenv.Load(".env", "./.env")
+	if err != nil {
+		// Не блокируем выполнение, если env прокинут через Docker напрямую
+		log.Info("No .env found")
+	}
+
+	db, err := database.NewPostgress()
+	if err != nil {
+		log.Error(err.Error())
+	}
+	defer db.Close()
+
+	// Make sure it works.
+	err = db.Ping()
+	if err != nil {
+		log.Error(err.Error())
+	}
 
 	// db init
+	err = database.Init(db)
+	if err != nil {
+		log.Error(err.Error())
+	}
 	// test register and test read
+
 }
--- services/account/internal/database/postgres.go
+++ services/account/internal/database/postgres.go
@@ -1,1 +1,66 @@
 package database
+
+import (
+	"database/sql"
+	"os"
+	"strconv"
+
+	"github.com/lib/pq"
+)
+
+func NewPostgress() (*sql.DB, error) {
+	cfg, err := pq.NewConfig("")
+	if err != nil {
+		return nil, err
+	}
+	cfg.SSLMode = "disable"
+
+	cfg.Host = os.Getenv("DB_HOST")
+	if portStr := os.Getenv("DB_PORT"); portStr != "" {
+		port, err := strconv.Atoi(portStr)
+		if err != nil {
+			return nil, err
+		}
+		cfg.Port = uint16(port)
+	}
+	cfg.Database = os.Getenv("DB_NAME")
+
+	cfg.User = os.Getenv("DB_USER")
+	cfg.Password = os.Getenv("DB_PASSWORD")
+
+	c, err := pq.NewConnectorConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create connection pool.
+	db := sql.OpenDB(c)
+
+	return db, nil
+
+}
+
+func Init(db *sql.DB) error {
+	query := `
+CREATE TABLE IF NOT EXISTS accounts (
+	id BIGSERIAL PRIMARY KEY,
+	email TEXT UNIQUE NOT NULL,
+	password_hash TEXT NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS profiles (
+	account_id BIGINT PRIMARY KEY,
+	username TEXT UNIQUE NOT NULL,
+	display_name TEXT NOT NULL,
+	bio TEXT NOT NULL DEFAULT '',
+	avatar_url TEXT NOT NULL DEFAULT '',
+
+	FOREIGN KEY(account_id) 
+	REFERENCES accounts(id) 
+	ON DELETE CASCADE
+);
+`
+	_, err := db.Exec(query)
+	return err
+}
